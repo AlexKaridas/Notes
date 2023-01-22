6b01892259802725ba79dd5e6d108aab594bdfc3
Alex <karidasalex@gmail.com>
2023-01-22 15:37:28 +0200

    1

---
+++ src/app/layout.jsx
@@ -0,0 +1,17 @@
+import './globals.css'
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+      <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600&display=swap" rel="stylesheet" precedence="default"></link>
+      <head />
+      <body>{children}</body>
+    </html>
+  )
+}
