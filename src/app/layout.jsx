297000293f648f57424d21eb60a0d59a354e2667
Alex <karidasalex@gmail.com>
2023-01-22 15:48:15 +0200

    1.2

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -7,9 +7,7 @@ export default function RootLayout({ children }) {
         <head /> will contain the components returned by the nearest parent
         head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-      <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600&display=swap" rel="stylesheet" precedence="default"></link>
+
       <head />
       <body>{children}</body>
     </html>
